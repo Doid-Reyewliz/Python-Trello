e3ca5f053007c20354e5cdbb81d269134ad1e8b8
lux_gero <60170240+Doid-Reyewliz@users.noreply.github.com>
2024-02-28 01:28:34 +0600

    Update settings.py

--- own_trello/own_trello/settings.py
+++ own_trello/own_trello/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'board.apps.BoardConfig',
     'login.apps.LoginConfig',
-    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -53,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'own_trello.urls'
